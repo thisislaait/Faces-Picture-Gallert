b6e442fe56ef088d3430a12314c3607723079a19
thisislaait <140851946+thisislaait@users.noreply.github.com>
2023-07-30 06:05:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {event.target.style.background ="rgb(58, 34, 16)"
+event.target.style.color = "white"
+
+})
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) => {event.target.style.background ="rgb(6, 88, 118)"
+event.target.style.color = "white"
+
+})
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {event.target.style.background ="rgb(234, 82, 0)"
+event.target.style.color = "white"
+
+})
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {event.target.style.background ="rgb(75, 74, 74)"
+event.target.style.color = "white"
+
+})
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {event.target.style.background ="black"
+event.target.style.color = "white"
+
+})
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {event.target.style.background ="saddlebrown"
+event.target.style.color = "white"
+
+})
